6aaa3ca9e37431e4de504cbabb2728403d235dfd
vadimtemov <128626319+vadimtemov@users.noreply.github.com>
2023-07-19 18:32:47 +0600

    Add files via upload

--- createx-2/js/main.js
+++ createx-2/js/main.js
@@ -12,4 +12,37 @@ $('.directions__filter-bth').on('click', function () {
     $(this).addClass('directions__filter-bth--active')
 })
 
+$('.team__slider').slick({
+    arrows: false,
+    slidesToShow: 4,
+    infinite: true,
+    draggable: false,
+})
+
+$('.team__slider-prew').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickPrev')
+})
+$('.team__slider-next').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickNext')
+})
+
+$('.testimonials__slider').slick({
+    arrows: false,
+    dots: true,
+    appendDots: $('.testimonials__dots'),
+    waitForAnimate: false,
+})
+
+$('.testimonials__prev').on('click', function (e) {
+    e.preventDefault()
+    $('.testimonials__slider').slick('slickPrev')
+})
+$('.testimonials__next').on('click', function (e) {
+    e.preventDefault()
+    $('.testimonials__slider').slick('slickNext')
+})
+
+
 })
