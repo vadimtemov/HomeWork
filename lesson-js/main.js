3f106bc65baf03c6663c921d036d8299b5cdd1b8
vadimtemov <128626319+vadimtemov@users.noreply.github.com>
2023-08-02 21:43:40 +0600

    Add files via upload

---
+++ lesson-js/main.js
@@ -0,0 +1,62 @@
+// ================== Урок 81 ===================================
+
+// const arr = [1, 33, 24, 66, 81, 50, 72]
+
+// for(let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 == 0) {
+
+//         console.log(arr[i])
+//     }
+
+// }  
+
+// --------------------------------------------------------------
+
+// let arr = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+
+// for(let i = 5; i >= 0; i--) {
+//     console.log(arr[i])
+// }
+
+// ================== Урок 82 ===================================
+
+// function greetings(name) {
+//     return 'hello ' + name
+// }
+
+// let result = greetings('Vadim')
+// console.log(result)
+
+// ----------------------------------------------------
+
+// const arr = [3, 8, 45, 9, 67, 87]
+
+// function number(a) {
+//     for(let i = 0; i < a.length; i++) {
+//         if (a[i] > 10) {
+//             return a[i]
+//         }
+//     }
+// }
+
+// console.log(number(arr))
+
+// ---------------------------------------------------
+
+
+// function calk(one, two, b) {
+//     if (b === 'sum') {
+//         return one + two 
+//     }
+//     if (b === 'minus') {
+//         return one - two
+//     }
+//     if (b === 'division') {
+//         return one / two 
+//     }
+//     else (b === 'mult') 
+//         return one * two
+// }
+
+// const result = calk(4, 3, 'sum')
+// console.log(result)
