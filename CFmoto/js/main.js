15c477ab0dc11d8896fd3260bbef3c66da031c38
vadimtemov <128626319+vadimtemov@users.noreply.github.com>
2023-09-08 20:02:06 +0600

    Add files via upload

--- CFmoto/js/main.js
+++ CFmoto/js/main.js
@@ -1,11 +1,30 @@
 const btn = document.querySelector('.connect_btn')
 const modal = document.querySelector('.body')
+const modalApp = document.querySelector('.modal_application')
 const btnClose = document.querySelector('.modal_application_close')
+
+
+const closeModalApp = () => {
+  modal.classList.remove('body_application-open')
+}
 
 btn.addEventListener('click', () => {
   modal.classList.add('body_application-open')
 })
 
+modalApp.addEventListener('click', event => {
+  const target = event.target
+  if (target && target.classList.contains('modal_application')) {
+    closeModalApp()
+  }
+})
+
 btnClose.addEventListener('click', () => {
-  modal.classList.remove('body_application-open')
+  closeModalApp()
+})
+
+document.addEventListener('keydown', event => {
+  if (event.code === 'Escape') {
+    closeModalApp()
+  }
 })
